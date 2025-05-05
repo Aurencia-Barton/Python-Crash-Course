d91db0fd8de225b6317b66f89ca3eb68d6f50382
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-05 12:53:31 -0500

    additional py files up to chap 5

---
+++ cars.py
@@ -0,0 +1,26 @@
+# cars = ['bmw', 'audi', 'tesla','toyota']
+# cars.sort() # permanenty sorts the cars list
+
+# print("\nHere's the original list: ")
+# print(cars)
+
+# print("\nHere's the sorted list: ")
+# print(sorted(cars))
+
+# len(cars)
+
+
+################################
+
+cars = ['audi', 'bmw', 'suburu', 'toyota']
+
+# for loop including an if statement to print the car, BMW.
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+
+
+
