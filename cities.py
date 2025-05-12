7e828247dcb4eb89567ddbaa08c7fc537dd61f96
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-12 11:56:06 -0500

    using breaks

---
+++ cities.py
@@ -0,0 +1,10 @@
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.)" # This line prompts the user to enter a city name or 'quit' to end the program.
+
+while True: # This loop continues indefinitely until the user enters 'quit'.
+    city = input(prompt)
+    
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!") # This line prints a message that includes the city name.
