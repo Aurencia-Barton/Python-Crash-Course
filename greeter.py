e473f566e91881b0313bcf87b089870840431df0
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-12 11:35:35 -0500

    testing input ptompts

---
+++ greeter.py
@@ -0,0 +1,9 @@
+# name = input("Please enter your name: ")
+# print(f"\nHello, {name}!")
+# # This program greets the user by name.
+
+
+prompt = "If you share your name, we can personalize the message you see."
+prompt+= "\n What is your first name?\n "
+name = input(prompt)
+print(f"\nHello, {name}!")
