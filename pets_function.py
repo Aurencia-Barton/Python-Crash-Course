915d43329e29a43ae3f7f585b4475407d061fcad
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-13 19:26:21 -0500

    working with keyword arguments

---
+++ pets_function.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type, animal_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.\n")
+    print(f"I have a {animal_type} named {animal_name.title()}.\n")
+describe_pet(animal_type='hamster', animal_name='harry')
+    
