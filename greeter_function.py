ab1860a165a78ddd5523ff6e06b995764ea5d879
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-13 19:18:44 -0500

    passing information to a function

--- greeter_function.py
+++ greeter_function.py
@@ -1,6 +1,6 @@
-def greet_user(): # defines a function definition named greet_user().
+def greet_user(username): # defines a function definition named greet_user().
     
     """Display a simple greeting message.""" # begins the body of the function with a docstring that describes what the function does.
-    print("Hello!") # Line of actual code that executes the function's task.
+    print(f"\nHello, {username.title()}\n") # Line of actual code that executes the function's task.
     
-greet_user() #it finally, calls the function greet_user() to execute the code within it.
+greet_user('jessie') #it finally, calls the function greet_user() to execute the code within it.
