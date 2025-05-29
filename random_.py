53e861ad4e2d0d23ca3718f3d100878979a56733
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-29 12:28:40 -0500

    using random module

---
+++ random_.py
@@ -0,0 +1,4 @@
+from random import randint
+
+randint(1, 6)  # Generates a random integer between 1 and 50
+print("Random integer between 1 and 6:", randint(1, 6))  # Prints the random integer generated
