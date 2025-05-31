7aec46c9ccffb1f2b8f5f0457a27314d48155838
Aurencia-Barton <aurenciabarton@gmail.com>
2025-05-31 09:52:04 -0500

    refactoring remember_me.py and adding get_stored_user() function

--- remember_me.py
+++ remember_me.py
@@ -1,17 +1,42 @@
 from pathlib import Path
 import json
 
+# def greet_user():
+#     """Greet the user by name.""" 
+# path = Path('username.json')
+# if path.exists():
+#     contents = path.read_text()
+#     username = json.loads(contents)
+#     print(f"Welcome back, {username}!")
+# else:
+#     username = input("\nWhat's is your name? ")
+#     contents = json.dumps(username)
+#     path.write_text(contents)
+#     print(f"\nWe'll remember you when you come back, {username}!")
+
+# greet_user()
+
+# refactored code for remember_me.py
+def get_stored_username(path):
+    """Get stored username if available."""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+    
 def greet_user():
     """Greet the user by name.""" 
-path = Path('username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("\nWhat's is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"\nWe'll remember you when you come back, {username}!")
+    path = Path('username.json')
+    username = get_stored_username(path)
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("\nWhat's is your name? ")
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"\nWe'll remember you when you come back, {username}!")
 
 greet_user()
+
